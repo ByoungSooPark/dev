b3a2c4cd4df2a6a7cec52cb47bae8a83f082023b
ByoungSooPark <jajangjajang@gmail.com>
2018-07-23 02:45:36 +0900

    first commit

---
+++ study00/Django/devEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/byoungsoopark/vue/dev/study00/Django/devEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ study00/Vue/example/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <button type=”button” name=”button” @click='getUsers'>get users</button>
+    <table>
+      <th>url</th>
+      <th>email</th>
+      <th>staff</th>
+      <tbody>
+      <tr v-for="elem in users" :key="elem.key">
+        <td><a :href="elem.url">{{ elem.username }}</a></td>
+        <td>{{ elem.email }}</td>
+        <td>{{ elem.is_staff }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    getUsers() {
+      axios.get('http:/127.0.0.1:8000/users/')
+        .then(res => this.users=res.data)
+        .catch(error => console.log(error))
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
